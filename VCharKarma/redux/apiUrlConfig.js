9576fe3f216998e64155d3e154c109a761a627fc
Jitendra Sah <jsah9105@gmail.com>
2018-09-25 07:47:52 -0500

    Url

--- VCharKarma/redux/apiUrlConfig.js
+++ VCharKarma/redux/apiUrlConfig.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export default baseURL = axios.create({
   //for local
-  baseURL: 'http://localhost:3000/api/'
-
+  //baseURL: 'http://localhost:3000/api/'
+  
   //for server
-  //baseURL: 'http://35.237.110.220:3001/api/'
+  baseURL: 'http://35.237.110.220:3001/api/'
 });
 
