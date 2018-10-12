ecea16074ecab143c17991c6d1b98dacb68e3a4d
Jitendra Sah <jsah9105@gmail.com>
2018-10-12 06:54:18 -0500

    Url Update

--- VCharKarma/redux/apiUrlConfig.js
+++ VCharKarma/redux/apiUrlConfig.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 
 const baseURL = axios.create({
   //for local host
-  baseURL: 'http://localhost:3000/api/'
+  //baseURL: 'http://localhost:3000/api/'
   
   //for server
- //baseURL: 'http://35.237.110.220:3001/api/'
+  baseURL: 'http://35.237.110.220:3001/api/'
 });
 
 //for local host
-const webSocketUrl = 'http://localhost:4000';
+//const webSocketUrl = 'http://localhost:4000';
 
 //for dev server 
-//const webSocketUrl = '35.237.110.220:4000';
+const webSocketUrl = 'http://35.237.110.220:4000';
 
 export { baseURL, webSocketUrl };
 
