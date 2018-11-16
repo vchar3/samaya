af3529e7ce5219188e3990734c18d8b7bf155169
Jitendra Sah <jsah9105@gmail.com>
2018-11-16 07:18:38 -0600

    Change

--- VCharKarma/redux/apiUrlConfig.js
+++ VCharKarma/redux/apiUrlConfig.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 
 const baseURL = axios.create({
   //for local host
-  baseURL: 'http://localhost:3000/api/'
+  //baseURL: 'http://localhost:3000/api/'
   
   //for dev server
-  //baseURL: 'http://35.237.139.25:3000/api/'
+  baseURL: 'http://35.237.139.25:3000/api/'
 });
 
 //for local host
-const webSocketUrl = 'http://localhost:4000';
+//const webSocketUrl = 'http://localhost:4000';
 
 //for dev server 
-//const webSocketUrl = 'http://35.237.139.25:4000';
+const webSocketUrl = 'http://35.237.139.25:4000';
 
 export { baseURL, webSocketUrl };
 
