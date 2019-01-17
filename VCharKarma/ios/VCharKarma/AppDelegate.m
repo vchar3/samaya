50f987092642b61956b9bbd6f2cfe28b9ddc5a91
Jitendra Sah <jsah9105@gmail.com>
2019-01-17 08:15:07 -0600

    Splash screen

--- VCharKarma/ios/VCharKarma/AppDelegate.m
+++ VCharKarma/ios/VCharKarma/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import <React/RCTPushNotificationManager.h>
+#import "RNSplashScreen.h"
 
 @implementation AppDelegate
 
@@ -30,6 +31,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];
   return YES;
 }
 
